8d09f8f790e7395e25664051b9d8092598d69c26
Bill Cawthra <bill@siliconharborlabs.com>
2017-11-13 14:20:34 -0500

    initial commit

---
+++ aws_credentials.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+CREDENTIAL_FILE=~/.aws/config
+
+# Get some variables with sed/awk.
+# The bracketed field should be the subscription ID.
+export AWS_ACCESS_KEY_ID=`cat $CREDENTIAL_FILE | grep -v "^#.*" | grep aws_access_key_id | awk '{ print $NF }'`
+export AWS_SECRET_ACCESS_KEY=`cat $CREDENTIAL_FILE | grep -v "^#.*" | grep aws_secret_access_key | awk '{ print $NF }'`
+
